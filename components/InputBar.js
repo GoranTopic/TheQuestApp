9a9b76f695d8ab36f5f21adeaafddee8e26a29de
telix <telegonicaxx@live.com>
2020-01-05 19:19:40 -0500

    Added so it can only add upper case

--- components/InputBar.js
+++ components/InputBar.js
@@ -43,7 +43,7 @@ export default class InputBar extends React.Component {
   _create = () => {
     this.props._createQuest(
       {
-        title: this.state.inputbuff,
+        title: this.state.inputbuff.toUpperCase(),
         shield: this.state.selectedShiled,
         selected: true,
         done: false,
