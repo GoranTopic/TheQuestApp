9cfde38f0a63573bb78107ee27044e9624d4a243
telix <telegonicaxx@live.com>
2022-10-19 02:16:22 -0700

    I'm guessing this may start causing us problems either soon or never.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,43 +1,43 @@
-);
-  },
-};
-const SettingsStack = createStackNavigator(
-import SettingsScreen from '../screens/SettingsScreen';
 import React from 'react';
-import ToDoScreen from '../screens/ToDoScreen';
-  ),
-
-
-
-
-
-
-
-
-
-
-
-import TabBarIcon from '../components/TabBarIcon';
-  {
-import { Platform } from 'react-native';
-export default tabNavigator;
-import { createBottomTabNavigator, createMaterialTopTabNavigator, createTopTabNavigator } from 'react-navigation-tabs';
-  todo: ToDoScreen, 
-  web: {},
-    Settings: SettingsScreen,
-  SettingsStack,
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-const tabNavigator = createMaterialTopTabNavigator({
-import LinksScreen from '../screens/LinksScreen';
-  default: {},
-import { createStackNavigator } from 'react-navigation-stack';
-const config = Platform.select({
-SettingsStack.path = '';
   config
 tabNavigator.path = '';
+const tabNavigator = createMaterialTopTabNavigator({
+const SettingsStack = createStackNavigator(
+  SettingsStack,
+  {
 import HomeScreen from '../screens/HomeScreen';
+export default tabNavigator;
+  tabBarLabel: 'Settings',
+    Settings: SettingsScreen,
+import ToDoScreen from '../screens/ToDoScreen';
+  ),
+import LinksScreen from '../screens/LinksScreen';
     <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+import TabBarIcon from '../components/TabBarIcon';
+import { createBottomTabNavigator, createMaterialTopTabNavigator, createTopTabNavigator } from 'react-navigation-tabs';
+  tabBarIcon: ({ focused }) => (
+import SettingsScreen from '../screens/SettingsScreen';
+  web: {},
 SettingsStack.navigationOptions = {
+
+
+
+
+
+
+
+
+
+
+
+};
 });
 });
+  default: {},
+const config = Platform.select({
+import { createStackNavigator } from 'react-navigation-stack';
+  todo: ToDoScreen, 
+import { Platform } from 'react-native';
+SettingsStack.path = '';
+);
+  },
